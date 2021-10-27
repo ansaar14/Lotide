d2b4366b861a09def514cc6ee309a26acb4960c2
Ansaar <Ansaar.ahmed245@gmail.com>
2021-10-27 22:10:12 +0000

    countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(word) {
+  const count = {};
+     
+  for (let letter of word) {
+      if (count[letter]) {
+        count[letter] += 1;
+      } else {
+        count[letter] = 1;
+      }
+    
+    console.log(letter);
+  }
+
+  return count;
+
+};
+
+countLetters("LHL")
