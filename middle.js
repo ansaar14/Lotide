9639ca2aa8c43ec7cd2df93c21c4b7049c76b491
Ansaar <Ansaar.ahmed245@gmail.com>
2021-10-27 18:17:26 +0000

    middle.js

--- middle.js
+++ middle.js
@@ -6,13 +6,13 @@ function middle(arr) {
   
     //For arrays with an even number of elements, an array containing the two elements in the middle should be returned //
   } else if (arr.length % 2 === 0) {
-    return [array[Math.round(arr.length / 2) - 1], array[Math.round(arr.length / 2)]];
+    return [arr[Math.round(arr.length / 2) - 1], arr[Math.round(arr.length / 2)]];
    
 
     // For arrays with odd number of elements, an array containing a single middle element should be returned. //
 
   } else {
-    return [array[Math.floor(arr.length / 2)]];
+    return [arr[Math.floor(arr.length / 2)]];
   }
   
 }
