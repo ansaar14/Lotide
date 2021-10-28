bcd4435e1f64097271f62d6f30a37d8eb36125d8
Ansaar <Ansaar.ahmed245@gmail.com>
2021-10-28 23:34:54 +0000

    takeUntil.js

---
+++ takeUntil.js
@@ -0,0 +1,54 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Implement takeUntil which will keep collecting items from a provided array until the callback provided returns a truthy value.
+
+const takeUntil = (array, callback) => {
+  let results = [];
+  for (let i of array) {
+    if (!callback(i)) {
+      results.push(i);
+    } else {
+      return results;
+    }
+  }
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, (x) => x < 0);
+console.log(results1);
+console.log(assertArraysEqual(results1, [1, 2, 5, 7, 2]));
+
+console.log("---");
+
+const data2 = [
+  "I've",
+  "been",
+  "to",
+  "Hollywood",
+  ",",
+  "I've",
+  "been",
+  "to",
+  "Redwood",
+];
+const results2 = takeUntil(data2, (x) => x === ",");
+console.log(results2);
+console.log(assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]));
