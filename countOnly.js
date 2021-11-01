340bc5f5fa59fe4c9fb194f783afb3f4895ae700
Ansaar <Ansaar.ahmed245@gmail.com>
2021-11-01 20:36:02 +0000

    countOnly.js

--- countOnly.js
+++ countOnly.js
@@ -12,7 +12,7 @@ const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
   for (const item of allItems) {
-    if (itemsToCount[item]) { 
+    if (itemsToCount[item]) {
       if (results[item]) {
         results[item] += 1;
       } else {
@@ -24,7 +24,7 @@ const countOnly = function(allItems, itemsToCount) {
 
   return results;
 
-}
+};
 const firstNames = [
   "Karl",
   "Salima",
@@ -44,4 +44,4 @@ assertEqual(result1["Karima"], undefined);
 assertEqual(result1["Fang"], 2);
 assertEqual(result1["Agouhanna"], undefined);
 
-module.exports = countOnly
+module.exports = countOnly;
