e02a9094880988a1d906759dc5b7fea80926a68c
Ansaar <Ansaar.ahmed245@gmail.com>
2021-11-01 19:31:48 +0000

    middleTest.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,22 @@
-const assertArraysEqual = require('../assertArraysEqual');
+
+const assert = require('chai').assert;
 const middle = require('../middle');
 
+describe("#middle", () => {
+  it("returns [1, 2, 3] for [2]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
 
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]) // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [2, 3])
+
+});
+
+
+
+
+
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]) // => [2, 3]
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [2, 3])
