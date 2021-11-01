c2c98eb6b55c7d4b56e0521a129d68a45fda20bb
Ansaar <Ansaar.ahmed245@gmail.com>
2021-11-01 17:06:52 +0000

    head.js

--- head.js
+++ head.js
@@ -1,19 +1,10 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
-  }
-};
-
-
+const assertEqual = require('./assertEqual');
 
 const head = function(array) {
 
   return array[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-
+// console.log(assertEqual(head([5,6,7]), 5));
  
+module.exports = head;
